c847fefb4726189ce6cabafde076710fd5197ddd
Olivier Ramonat <olivier@ramonat.fr>
2007-12-14 14:54:44 +0100

    Add No_Server, Q_Key_Pressed options
    Default is to wait forever

--- argwiad/src/argwiad.adb
+++ argwiad/src/argwiad.adb
@@ -19,28 +19,83 @@
 --  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
 ------------------------------------------------------------------------------
 
-with Gwiad.Web;
-with Gwiad.Dynamic_Libraries.Manager;
-with Ada.Text_IO;
+with Ada.Command_Line;
 with Ada.Exceptions;
+with Ada.Text_IO;
+
+with AWS.Server;
+
+with GNAT.Case_Util;
+
+with Gwiad.Dynamic_Libraries.Manager;
+with Gwiad.Web;
 
 procedure Argwiad is
 
-   use Gwiad;
+   use Ada;
    use Ada.Exceptions;
+   use Ada.Command_Line;
+   use GNAT;
+   use Gwiad;
 
-   Discover_Library : Dynamic_Libraries.Manager.Discover;
+   type Cmd_Options is (No_Server, Q_Key_Pressed);
+
+   procedure Usage;
+   --  Displays usage
+
+   -----------
+   -- Usage --
+   -----------
+
+   procedure Usage is
+   begin
+      Text_IO.Put ("usage: argwiad ");
+      for Opt in Cmd_Options'Range loop
+         declare
+            Option_Name : String := Cmd_Options'Image (Opt);
+         begin
+            GNAT.Case_Util.To_Lower (Option_Name);
+            if Opt /= Cmd_Options'Last then
+               Text_IO.Put ("-" & Option_Name & "|");
+            else
+               Text_IO.Put_Line (Option_Name);
+            end if;
+         end;
+      end loop;
+   end Usage;
+
+   Termination : AWS.Server.Termination := AWS.Server.Forever;
 
 begin
+   if Argument_Count /= 0 then
+      for K in 1 .. Argument_Count loop
+         if Argument (K) = "-h" or else Argument (K) = "-help"
+           or else Argument (K) = "--help" then
+            Usage;
+            return;
 
-   Web.Start;
+         elsif Argument (K)'Length > 1 then
+            case Cmd_Options'Value (Argument (K) (2 .. Argument (K)'Length)) is
+            when No_Server | Q_Key_Pressed =>
+                  Termination := AWS.Server.Termination'Value
+                    (Argument (K) (2 .. Argument (K)'Length));
+            end case;
+         end if;
+      end loop;
+   end if;
 
-   Web.Wait;
+   Start_Argwiad : declare
+      Discover_Library : Dynamic_Libraries.Manager.Discover;
+   begin
+      Web.Start;
 
-   --  Then exit
+      Web.Wait (Termination);
 
-   Web.Stop;
-   Discover_Library.Stop;
+      --  Then exit
+
+      Web.Stop;
+      Discover_Library.Stop;
+   end Start_Argwiad;
 
 exception
    when E : others =>
